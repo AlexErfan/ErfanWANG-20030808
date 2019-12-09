0d565264fbc14bc3cc739893c7f4438f5ec78326
Alex <1014104338@qq.com>
2019-12-09 21:08:49 +0800

    add start button to pacman game without any css

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,38 @@
 package sample;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class Controller {
+	@FXML
+	private Button start;
+	
+	public void changeSecene() {
+		
+		Stage theStage = new Stage();
+		Group root = new Group();
+		Scene theScene = new Scene( root );
+	    theStage.setScene(theScene );
+
+	    Canvas canvas = new Canvas( 1225, 600 );
+	    root.getChildren().add( canvas );
+	    GameManager gameManager = new GameManager(root);
+
+	    gameManager.drawBoard();
+	         
+
+	    theScene.addEventHandler(KeyEvent.KEY_PRESSED, event -> gameManager.movePacman(event));
+	    theScene.addEventHandler(KeyEvent.KEY_RELEASED, event -> gameManager.stopPacman(event));
+	    theScene.addEventHandler(KeyEvent.KEY_PRESSED, event -> gameManager.restartGame(event));
+
+	    theStage.show();
+	}
+	
 }
