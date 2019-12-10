f34ae2c1f5267c9b74233c5ef64e0197644a3685
Alex <1014104338@qq.com>
2019-12-10 14:40:47 +0800

    change Controller.java to startController.java

--- src/sample/startController.java
+++ src/sample/startController.java
@@ -1,22 +1,29 @@
 package sample;
 
+import java.io.IOException;
+
+import com.sun.corba.se.impl.orbutil.graph.Node;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
-public class Controller {
+public class startController {
 	@FXML
-	private Button start;
+	private Button start, setup;
 	
-	public void changeSecene() {
+	public void changeSecene(ActionEvent e) {
 		
 		Stage theStage = new Stage();
+//		Stage theStage = (Stage) ((Button) e.getSource()).getScene().getWindow();
 		Group root = new Group();
 		Scene theScene = new Scene( root );
 	    theStage.setScene(theScene );
@@ -34,5 +41,18 @@ public class Controller {
 
 	    theStage.show();
 	}
+	
+	public void goToSetup(ActionEvent event) throws IOException {
+		Parent setting = FXMLLoader.load(getClass().getResource("setup.fxml"));
+		Scene setupScene = new Scene(setting);
+			
+		// this line gets the stage information
+		Stage setupWindow = (Stage) ((Button) event.getSource()).getScene().getWindow();
+		setupWindow.setScene(setupScene);
+		setupWindow.show();
+		System.out.println("goToSetup");
+	}
+	
+	
 	
 }
